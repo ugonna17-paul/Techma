ed8ec74b89656d1687bcb171384a1829c1b2aebb
ugonna17-paul <ugonnaaninwodo@gmail.com>
2025-09-10 00:09:36 +0100

    Updated timeline responsiveness and redeploy setup

--- my-techma/src/App.jsx
+++ my-techma/src/App.jsx
@@ -2,21 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './pages/Landing-page/home'
+// import Home from './pages/Landing-page/home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from './pages/About-us/About'
+// import Home from './pages/Landing-page/home'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <Home /> */}
+    
       <About />
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
-          <Route />
+          <Route path="/" element={<About />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   )
 }
